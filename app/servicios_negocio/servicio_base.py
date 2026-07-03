82f0c6dfc5eb9cd6d8ddf203d31c4d0599d2706a
Kthiu666 <kathlissa6@gmail.com>
2026-07-03 01:27:43 -0500

    refactor: eliminar archivos redundantes

--- app/servicios_negocio/servicio_base.py
+++
@@ -1,9 +0,0 @@
-# Este archivo demuestra cómo se estructurarán los servicios futuros
-class ServicioBase:
-    """
-    Clase base para todos los servicios de negocio.
-    Aquí se inyectará la sesión de la base de datos (Unit of Work / Repositorio)
-    para que la lógica de negocio no interactúe directamente con SQLAlchemy.
-    """
-    def __init__(self, repositorio=None):
-        self.repositorio = repositorio3
